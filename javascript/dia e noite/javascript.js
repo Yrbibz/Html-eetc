ff8c0cc4893a5a40c4ff80c6f4a1eeb0ee9d2bd5
Yrbibz <yrbibz.who@gmail.com>
2025-02-05 15:12:26 -0300

    javascript

--- javascript/dia e noite/javascript.js
+++ javascript/dia e noite/javascript.js
@@ -1,3 +1,27 @@
-function carregar(){
+function carregar() {
+    let agora = new Date();
+    let hora = agora.getHours();
+    let minuto = agora.getMinutes();
+    let horas = document.getElementById('horas');
+    let img = document.getElementById('imagem');
+    let corpo = document.body; 
+
+    horas.innerText = `Agora são ${hora}:${minuto < 10 ? '0' + minuto : minuto}`;
+
+    if (hora >= 6 && hora < 12) {
+        horas.innerText += " - Bom dia!";
+        img.src = 'estilo/manha.jpg'
+        corpo.style.background = "#F6DABE"
     
+    }
+    else if(hora >=13 && hora <=18){
+        horas.innerText += " - Boa Tarde!";
+        img.src = "estilo/tarde.jpg"
+        corpo.style.background = "#E67B3C"; 
+    }
+    else {
+        horas.innerText += " - Boa noite!";
+        img.src = "estilo/noite.jpg"
+        corpo.style.background = "#373C7C"; 
+    }
 }
