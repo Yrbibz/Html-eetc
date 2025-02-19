c3f46c79b4e83cf7cbd9897bc44f093119f6da7f
Yrbibz <yrbibz.who@gmail.com>
2025-02-19 17:10:40 -0300

    att

--- jogo/script/script.js
+++ jogo/script/script.js
@@ -5,20 +5,28 @@ function ajustarPalco() {
     //Guardando valores de altura e largura da tela
     altura = window.innerHeight;
     largura = window.innerWidth;
-    console.log(largura, altura);
+    console.log("Largura:", largura, "Altura:", altura)
 
 }
 
 ajustarPalco();
 
 function posRandomica(){
+
+    //remover o papaleguas caso ja exista
+    if(document.getElementById('tempoPL)')){ 
+        document.getElementById('tempoPL').remove();
+
+    document.getElementById("v1").src = "imgs/coracaovazio.png";
+}
+
     //Randomizando posição do papaleguas na tela
     var posx = Math.floor(Math.random() * largura)-100;
     var posy = Math.floor(Math.random() * altura)-100;
 
     //Validador para que não tenha posições negativas
-    posx = posx < 0 ? 0 : posx;
-    posx = posy < 0 ? 0 : posy;
+    posx = posx < 0 ? 0: posx;
+    posy = posy < 0 ? 0: posy;
     console.log(posx, posy);
 
     //Criando o elemento na tela
@@ -29,23 +37,41 @@ function posRandomica(){
     document.body.appendChild(papaleguas);
 
 
-    papaleguas.className = 'papaleguas';
+    papaleguas.className = tamanhoPapaleguas()+ ' '+ ladoAleatorio();
     papaleguas.style.left = `${posx}px`;
     papaleguas.style.top = `${posy}px`;
     papaleguas.style.position = 'absolute';
+    papaleguas. ='tempoPL';
 
-    console.log(tamanhoPapaleguas());
+    console.log(ladoAleatorio());
+
+    papaleguas.onclick = function() {
+        this.remove();
+    }
 }
 
+
+
 function tamanhoPapaleguas(){
-    var classe=Math.floor(Math.random()*3);
+    var classe = Math.floor(Math.random() * 3);
     
     switch(classe){
         case 0:
-            return 'pl1';
+            return "pl1";
         case 1:
-            return 'pl2';
+            return "pl2";
         case 2:
-            return 'pl3';
+            return "pl3";
+    }
+}
+
+function ladoAleatorio(){
+    var classe=Math.floor(Math.random()*2);
+    
+    switch(classe){
+        case 0:
+            return 'ladoA';
+        case 1:
+            return 'ladoB';
     }
 }
